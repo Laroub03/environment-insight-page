930ec0e3357ab7c767be3a7d1e21978f8b910116
emil <emil@emil>
2024-05-08 08:38:06 +0200

    Updated stuff. 🦄

--- src/app/views/dashboard/dashboard-charts-data.ts
+++ src/app/views/dashboard/dashboard-charts-data.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 import {
   ChartData,
   ChartDataset,
@@ -6,15 +6,15 @@ import {
   ChartType,
   PluginOptionsByType,
   ScaleOptions,
-  TooltipLabelStyle
-} from 'chart.js';
-import { DeepPartial } from 'chart.js/dist/types/utils';
-import { getStyle, hexToRgba } from '@coreui/utils';
-import { SensorDataService } from '../../services/sensor-Data.service';
-import { ClientData } from '../../interfaces/clientData';
+  TooltipLabelStyle,
+} from "chart.js";
+import { DeepPartial } from "chart.js/dist/types/utils";
+import { getStyle, hexToRgba } from "@coreui/utils";
+import { SensorDataService } from "../../services/sensor-Data.service";
+import { ClientData } from "../../interfaces/clientData";
 
 interface SensorRanges {
-  [key: string]: { min: number, max: number, stepSize: number };
+  [key: string]: { min: number; max: number; stepSize: number };
 }
 
 export interface IChartProps {
@@ -25,7 +25,7 @@ export interface IChartProps {
 }
 
 @Injectable({
-  providedIn: 'any'
+  providedIn: "any",
 })
 export class DashboardChartsData {
   private sensorRanges: SensorRanges = {
@@ -33,78 +33,143 @@ export class DashboardChartsData {
     altitude: { min: 0, max: 5000, stepSize: 1000 },
     pressure: { min: 900, max: 1100, stepSize: 40 },
     temperature: { min: -50, max: 50, stepSize: 20 },
-    light: { min: 0, max: 1023, stepSize: 20000 }
+    light: { min: 0, max: 1023, stepSize: 20000 },
   };
-  
-  public mainChart: IChartProps = { type: 'line', labels: [], options: {} };
+
+  public mainChart: IChartProps = { type: "line", labels: [], options: {} };
 
   constructor(private sensorDataService: SensorDataService) {
     this.initMainChart();
   }
 
-  private generateRandomData(elementCount: number, min: number, max: number): number[] {
-    return Array.from({ length: elementCount }, () => Math.floor(Math.random() * (max - min + 1) + min));
-  }
-
-  private getChartColors(): { backgroundColor: string, borderColor: string, pointHoverBackgroundColor: string, borderWidth: number, fill: boolean } {
-    const brandInfo = getStyle('--cui-info') ?? '#20a8d8';
+  private getChartColors(): {
+    backgroundColor: string;
+    borderColor: string;
+    pointHoverBackgroundColor: string;
+    borderWidth: number;
+    fill: boolean;
+  } {
+    const brandInfo = getStyle("--cui-info") ?? "#20a8d8";
     const brandInfoBg = hexToRgba(brandInfo, 10);
     return {
       backgroundColor: brandInfoBg,
       borderColor: brandInfo,
       pointHoverBackgroundColor: brandInfo,
       borderWidth: 2,
-      fill: true
+      fill: true,
     };
   }
 
-  extractValuesAndRange(property: keyof ClientData): { values: any[], range: number } {
-    var clientData: ClientData [] = [];
-    this.sensorDataService.clientsData$.subscribe((data) => clientData = data);
-    let values: any[] = clientData.map(data => data[property]);
+  extractValuesAndRange(property: keyof ClientData): {
+    values: any[];
+    range: number;
+  } {
+    var clientData: ClientData[] = [];
+    this.sensorDataService.clientsData$.subscribe(
+      (data) => (clientData = data),
+    );
+    let values: any[] = clientData.map((data) => data[property]);
     let min = Math.min(...values);
     let max = Math.max(...values);
     let range = max - min;
     return { values, range };
   }
 
-  public initMainChart(period: string = 'Month') {
+  public initMainChart(
+    period: string = "Month",
+    selectedSensor: string = "humidity",
+  ) {
+    var values: number[] = [];
+    var clientData: ClientData[] = [];
+    this.sensorDataService.clientsData$.subscribe(
+      (data) => (clientData = data),
+    );
+
+    // Iterate over the clientDataList using a for loop
+    for (const data of clientData) {
+      // Extract the humidity value from each ClientData object and push it to the humidityList
+      switch (selectedSensor) {
+        case "humidity":
+          values.push(data.humidity);
+          break;
+
+        case "altitude":
+          values.push(data.altitude);
+          break;
+
+        case "pressure":
+          values.push(data.pressure);
+          break;
+
+        case "temperature":
+          values.push(data.temperature);
+          break;
+
+        case "light":
+          values.push(data.light);
+          break;
+
+        default:
+          values.push(data.temperature);
+          break;
+      }
+    }
+
     const colors = this.getChartColors();
     const labels = this.getLabelsForPeriod(period);
-    const datasets: ChartDataset[] = [];
+    const data = values;
 
-    // Define properties and titles directly within the loop
-    const properties: (keyof ClientData)[] = ['humidity', 'altitude', 'pressure', 'temperature', 'light'];
-    const titles: string[] = ['Humidity', 'Altitude', 'Pressure', 'Temperature', 'Light'];
-
-    // Manually populate datasets for each property and title
-    for (let i = 0; i < properties.length; i++) {
-        const property = properties[i];
-        const title = titles[i];
-
-        let { values, range } = this.extractValuesAndRange(property);
-
-        const dataset: ChartDataset = {
-            data: values,
-            label: title,
-            ...colors
-        };
-
-        datasets.push(dataset);
-    }
+    const datasets: ChartDataset[] = [
+      {
+        data: data,
+        label: "Current",
+        ...colors,
+      },
+    ];
 
     this.mainChart.data = { datasets, labels };
     this.mainChart.options = this.getChartOptions();
-}
+  }
 
   private getLabelsForPeriod(period: string): string[] {
     switch (period) {
-      case 'Year':
-        return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-      case 'Day':
+      case "Year":
+        return [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ];
+      case "Day":
         return Array.from({ length: 24 }, (_, i) => `${i}:00`);
       default:
-        return ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].concat(...Array(3).fill(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']));
+        return [
+          "Monday",
+          "Tuesday",
+          "Wednesday",
+          "Thursday",
+          "Friday",
+          "Saturday",
+          "Sunday",
+        ].concat(
+          ...Array(3).fill([
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+          ]),
+        );
     }
   }
 
@@ -112,11 +177,16 @@ export class DashboardChartsData {
     return {
       maintainAspectRatio: false,
       plugins: this.getPlugins(),
-      scales: this.getScales('defaultSensorType'),
+      scales: this.getScales("defaultSensorType"),
       elements: {
         line: { tension: 0.4 },
-        point: { radius: 0, hitRadius: 10, hoverRadius: 4, hoverBorderWidth: 3 }
-      }
+        point: {
+          radius: 0,
+          hitRadius: 10,
+          hoverRadius: 4,
+          hoverBorderWidth: 3,
+        },
+      },
     };
   }
 
@@ -125,14 +195,21 @@ export class DashboardChartsData {
       legend: { display: false },
       tooltip: {
         callbacks: {
-          labelColor: (context) => ({ backgroundColor: context.dataset.borderColor } as TooltipLabelStyle)
-        }
-      }
+          labelColor: (context) =>
+            ({
+              backgroundColor: context.dataset.borderColor,
+            }) as TooltipLabelStyle,
+        },
+      },
     };
   }
 
   getScales(sensorType: string): ScaleOptions<any> {
-    const range = this.sensorRanges[sensorType] || { min: 0, max: 250, stepSize: 50 };
+    const range = this.sensorRanges[sensorType] || {
+      min: 0,
+      max: 250,
+      stepSize: 50,
+    };
     return {
       y: {
         beginAtZero: true,
@@ -140,9 +217,9 @@ export class DashboardChartsData {
         min: range.min,
         ticks: {
           stepSize: range.stepSize,
-          maxTicksLimit: 5
-        }
-      }
+          maxTicksLimit: 5,
+        },
+      },
     };
-  }  
+  }
 }
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,7 +1,20 @@
-import { DOCUMENT, NgStyle } from '@angular/common';
-import { CommonModule } from '@angular/common';
-import { Component, DestroyRef, effect, inject, OnInit, Renderer2, signal, WritableSignal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { DOCUMENT, NgStyle } from "@angular/common";
+import { CommonModule } from "@angular/common";
+import {
+  Component,
+  DestroyRef,
+  effect,
+  inject,
+  Renderer2,
+  signal,
+  WritableSignal,
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import {
   AvatarComponent,
   ButtonDirective,
@@ -17,45 +30,75 @@ import {
   ProgressComponent,
   RowComponent,
   TableDirective,
-  TextColorDirective
-} from '@coreui/angular';
-import { ChartjsComponent } from '@coreui/angular-chartjs';
-import { IconDirective } from '@coreui/icons-angular';
+  TextColorDirective,
+} from "@coreui/angular";
+import { ChartjsComponent } from "@coreui/angular-chartjs";
+import { IconDirective } from "@coreui/icons-angular";
+import { FormsModule } from "@angular/forms";
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatFormFieldModule } from "@angular/material/form-field";
 
-import { WidgetsBrandComponent } from '../widgets/widgets-brand/widgets-brand.component';
-import { WidgetsDropdownComponent } from '../widgets/widgets-dropdown/widgets-dropdown.component';
-import { DashboardChartsData, IChartProps } from './dashboard-charts-data';
-import { ChartOptions } from 'chart.js';
-import { SensorDataService } from 'src/app/services/sensor-Data.service';
-import { formatDate } from '@angular/common';
-import { ClientData } from 'src/app/interfaces/clientData';
+import { WidgetsBrandComponent } from "../widgets/widgets-brand/widgets-brand.component";
+import { WidgetsDropdownComponent } from "../widgets/widgets-dropdown/widgets-dropdown.component";
+import { DashboardChartsData, IChartProps } from "./dashboard-charts-data";
+import { SensorDataService } from "src/app/services/sensor-Data.service";
+import { formatDate } from "@angular/common";
 
 @Component({
-  selector: 'app-dashboard-card',
-  templateUrl: 'dashboard.component.html',
-  styleUrls: ['dashboard.component.scss'],
+  selector: "app-dashboard-card",
+  templateUrl: "dashboard.component.html",
+  styleUrls: ["dashboard.component.scss"],
   standalone: true,
-  imports: [CommonModule, WidgetsDropdownComponent, TextColorDirective, CardComponent, CardBodyComponent, RowComponent, ColComponent, ButtonDirective, IconDirective, ReactiveFormsModule, ButtonGroupComponent, FormCheckLabelDirective, ChartjsComponent, NgStyle, CardFooterComponent, GutterDirective, ProgressBarDirective, ProgressComponent, WidgetsBrandComponent, CardHeaderComponent, TableDirective, AvatarComponent]
+  imports: [
+    CommonModule,
+    WidgetsDropdownComponent,
+    TextColorDirective,
+    CardComponent,
+    CardBodyComponent,
+    RowComponent,
+    ColComponent,
+    ButtonDirective,
+    IconDirective,
+    ReactiveFormsModule,
+    ButtonGroupComponent,
+    FormCheckLabelDirective,
+    ChartjsComponent,
+    NgStyle,
+    CardFooterComponent,
+    GutterDirective,
+    ProgressBarDirective,
+    ProgressComponent,
+    WidgetsBrandComponent,
+    CardHeaderComponent,
+    TableDirective,
+    AvatarComponent,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatFormFieldModule,
+  ],
 })
-export class DashboardComponent { 
+export class DashboardComponent {
   sensorData: any;
-  minDate: string = '';
-  maxDate: string = '';
+  minDate: string = "";
+  maxDate: string = "";
+  period: string = "Month";
+  selectedSensor: string = "humidity";
 
   public sensors = [
-    { id: 'humidity', title: 'Humidity', dateRange: '' },
-    { id: 'altitude', title: 'Altitude', dateRange: '' },
-    { id: 'pressure', title: 'Pressure', dateRange: '' },
-    { id: 'temperature', title: 'Temperature', dateRange: '' },
-    { id: 'light', title: 'Light', dateRange: '' }
+    { id: "humidity", title: "Humidity", dateRange: "100", values: [] },
+    { id: "altitude", title: "Altitude", dateRange: "1000", values: [] },
+    { id: "pressure", title: "Pressure", dateRange: "250000", values: [] },
+    { id: "temperature", title: "Temperature", dateRange: "100", values: [] },
+    { id: "light", title: "Light", dateRange: "1500", values: [] },
   ];
   readonly #destroyRef: DestroyRef = inject(DestroyRef);
   readonly #document: Document = inject(DOCUMENT);
   readonly #renderer: Renderer2 = inject(Renderer2);
   readonly #chartsData: DashboardChartsData = inject(DashboardChartsData);
 
-
-  public mainChart: IChartProps = { type: 'line' };
+  public mainChart: IChartProps = { type: "line" };
   public mainChartRef: WritableSignal<any> = signal(undefined);
   #mainChartRefEffect = effect(() => {
     if (this.mainChartRef()) {
@@ -63,33 +106,41 @@ export class DashboardComponent {
     }
   });
   public sensorDataGroup = new FormGroup({
-    clientId: new FormControl('', Validators.required), // Ensure client ID is not empty
-    fromDate: new FormControl('', Validators.required), // Ensure date is selected
-    toDate: new FormControl('', Validators.required), // Ensure date is selected
-    sensorData: new FormControl('Month') // Default to 'Month'
+    clientId: new FormControl("", Validators.required), // Ensure client ID is not empty
+    fromDate: new FormControl("", Validators.required), // Ensure date is selected
+    toDate: new FormControl("", Validators.required), // Ensure date is selected
+    sensorData: new FormControl("Month"), // Default to 'Month'
+    selectedSensor: new FormControl("", Validators.required), // Ensure a sensor is selected
   });
-  
-  constructor(private sensorDataService: SensorDataService) {}
 
-
+  constructor(private sensorDataService: SensorDataService) {
+    this.sensorDataGroup.valueChanges.subscribe((values) => {
+      this.#chartsData.initMainChart(
+        this.period,
+        values.selectedSensor as string,
+      );
+      this.selectedSensor = values.selectedSensor as string;
+      this.initCharts();
+      this.setChartStyles();
+    });
+  }
 
   ngOnInit(): void {
     this.initializeDateRange();
 
     this.initCharts();
     this.updateChartOnColorModeChange();
-
   }
 
   initializeDateRange(): void {
     const currentDate = new Date();
     const tomorrow = new Date(currentDate.getTime() + 24 * 60 * 60 * 1000); // Add one day to current date
 
-    this.maxDate = formatDate(tomorrow, 'yyyy-MM-dd', 'en-US'); // Allow selection up to tomorrow
+    this.maxDate = formatDate(tomorrow, "yyyy-MM-dd", "en-US"); // Allow selection up to tomorrow
 
     const lastYearDate = new Date(currentDate.getTime());
     lastYearDate.setFullYear(lastYearDate.getFullYear() - 1);
-    this.minDate = formatDate(lastYearDate, 'yyyy-MM-dd', 'en-US');
+    this.minDate = formatDate(lastYearDate, "yyyy-MM-dd", "en-US");
   }
 
   getMaxFromDate(): string {
@@ -105,15 +156,18 @@ export class DashboardComponent {
   onSubmit() {
     if (this.sensorDataGroup.valid) {
       const clientId = this.sensorDataGroup.value.clientId as string;
-      const fromDate = Math.floor(new Date(this.sensorDataGroup.value.fromDate as string).getTime() / 1000);
-      const toDate = Math.floor(new Date(this.sensorDataGroup.value.toDate as string).getTime() / 1000);
+      const fromDate = Math.floor(
+        new Date(this.sensorDataGroup.value.fromDate as string).getTime() /
+          1000,
+      );
+      const toDate = Math.floor(
+        new Date(this.sensorDataGroup.value.toDate as string).getTime() / 1000,
+      );
       console.log(fromDate);
 
       this.sensorDataService.fetchSensorData(clientId, fromDate, toDate);
-      this.sensorDataService.clientsData$.subscribe((data) => console.log(data));
     }
   }
-  
 
   // loadSensorData() {
   //   const clientId = 'yourClientId'; // This should be dynamically set based on user selection or another parameter
@@ -130,11 +184,10 @@ export class DashboardComponent {
   //   );
   // }
 
-
   updateDateRanges(): void {
     const currentYear = new Date().getFullYear();
     const dateRange = `January - December ${currentYear}`;
-    this.sensors.forEach(sensor => sensor.dateRange = dateRange);
+    this.sensors.forEach((sensor) => (sensor.dateRange = dateRange));
   }
 
   initCharts(): void {
@@ -142,29 +195,34 @@ export class DashboardComponent {
   }
 
   setSensorData(value: string): void {
-    if (this.sensorDataGroup.get('sensorData')?.value !== value) {
+    if (this.sensorDataGroup.get("sensorData")?.value !== value) {
       this.sensorDataGroup.patchValue({
-        sensorData: value
+        sensorData: value,
       });
-      this.#chartsData.initMainChart(value); // Initialize chart data based on the selected sensor
+      this.#chartsData.initMainChart(value, this.sensorData); // Initialize chart data based on the selected sensor
+      this.period = value;
       this.initCharts();
       this.setChartStyles(); // Update chart styles with the new configuration
     }
-  } 
-  
+  }
+
   handleChartRef($chartRef: any) {
     if ($chartRef) {
       this.mainChartRef.set($chartRef);
       this.setChartStyles(); // Immediately apply the correct styles
     } else {
-      console.error('Failed to set chart reference');
+      console.error("Failed to set chart reference");
     }
-  }  
-  
+  }
+
   updateChartOnColorModeChange() {
-    const unListen = this.#renderer.listen(this.#document.documentElement, 'ColorSchemeChange', () => {
-      this.setChartStyles();
-    });
+    const unListen = this.#renderer.listen(
+      this.#document.documentElement,
+      "ColorSchemeChange",
+      () => {
+        this.setChartStyles();
+      },
+    );
 
     this.#destroyRef.onDestroy(() => {
       unListen();
@@ -174,14 +232,14 @@ export class DashboardComponent {
   setChartStyles() {
     if (this.mainChartRef()) {
       setTimeout(() => {
-        const sensorType = this.sensorDataGroup.get('sensorData')?.value || 'defaultSensorType';
-        console.log('Sensor Type:', sensorType); // Check the sensor type being used
+        const sensorType =
+          this.sensorDataGroup.get("selectedSensor")?.value || "humidity";
+        console.log("Sensor Type:", sensorType); // Check the sensor type being used
         const scales = this.#chartsData.getScales(sensorType);
-        console.log('Scales being set:', scales); // Check the scales being calculated
+        console.log("Scales being set:", scales); // Check the scales being calculated
         this.mainChartRef().options.scales = scales;
         this.mainChartRef().update();
       }, 0); // timeout set to 0 for immediate execution
     }
   }
-  
 }
